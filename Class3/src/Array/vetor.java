dd6a61643dc958547c9aa8784bdd52ef4d4b1a54
Deborah Leite <dleitedias@gmail.com>
2023-02-25 23:11:04 -0300

    task 7: array

--- Class3/src/Array/vetor.java
+++ Class3/src/Array/vetor.java
@@ -4,8 +4,9 @@ public class vetor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String [] car = {"volvo","ford", "bmw", "ferrari"};
-		
+		String [] cars = {"volvo","ford", "bmw", "ferrari"};
+		cars[0] = "mercedees";
+		System.out.println(cars[0]);
 	}
 
 }
